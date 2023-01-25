94f7f502ae23f00b28889841e49d2b0ca09f980c
Ricardo <ricardo.spinola9@gmail.com>
2023-01-25 12:13:23 -0300

    teste ci/cd

--- src/pages/login.js
+++ src/pages/login.js
@@ -25,7 +25,7 @@ export default function Login() {
               alt="Your Company"
             />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign in to your account
+              Entrar com sua conta
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               Or{' '}
